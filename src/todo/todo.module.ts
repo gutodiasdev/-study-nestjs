72c2d0514ee54af8c8577d968b4c2a57827d7ba7
Augusto Dias <gutodiasux@gmail.com>
2022-11-26 21:54:16 -0300

    feat: add PrismaService

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
-import { TodoController } from './todo.controller';
 
 @Module({
   controllers: [TodoController],
-  providers: [TodoService]
+  providers: [TodoService],
 })
 export class TodoModule {}
